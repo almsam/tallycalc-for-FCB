a4b679416b72c3cd583cf53e7cd384272e929a95
almsam <44277999+almsam@users.noreply.github.com>
2024-05-07 13:34:50 -0700

    AS swift

---
+++ Advanced segues/Advanced segues/ViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ViewController.swift
+//  Advanced segues
+//
+//  Created by Rad almuallim on 2017-03-10.
+//  Copyright © 2017 Rad almuallim. All rights reserved.
+//
+
+import UIKit
+
+var rowCounter:Int = 0
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        
+        print(rowCounter)
+        
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
